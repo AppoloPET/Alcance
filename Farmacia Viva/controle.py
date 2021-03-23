96bb6b8f2407dd0b35c4f919116c6d9b2a1576cf
Guilherme Tsubahara Horstmann <guilhermehorstmann1@gmail.com>
2021-03-23 15:18:32 -0300

    controle dos frames

--- Farmacia Viva/controle.py
+++ Farmacia Viva/controle.py
@@ -1,6 +1,38 @@
 from PyQt5  import uic, QtWidgets
 
 
+class JanelaMain:
+    def __init__(self, app, janela) -> None:
+        self.app = app
+        self.janela = janela
+
+        self.botoes_principais()
+
+        janela.show()
+        self.app.exec()
+
+    def botoes_principais(self):
+        self.janela.botao_editar.clicked.connect(self.frame_editar)
+        self.janela.botao_ler.clicked.connect(self.frame_ler)
+        self.janela.botao_escrever.clicked.connect(self.frame_escrever)
+
+    def frame_editar(self):
+        self.janela.frame_escrever.close()
+        self.janela.frame_editar.show()
+
+    def frame_ler(self):
+        self.janela.frame_editar.close()
+        self.janela.frame_escrever.close()
+
+    def frame_escrever(self):
+        self.janela.frame_editar.show()
+        self.janela.frame_escrever.show()
+    
+
+JanelaMain(app=QtWidgets.QApplication([]), janela=uic.loadUi("Farmacia.ui"))
+
+
+
 
 
 
